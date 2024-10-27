568b4f37912b21483a280b2d3bcd64db4e2dc266
Joabe Alves <joabealvesyt@gmail.com>
2024-10-27 17:07:20 -0300

    git commit -m "interface adds"

--- aplicativo.py
+++ aplicativo.py
@@ -1,44 +1,84 @@
 import tkinter as tk
+from tkinter import messagebox
 import ttkbootstrap as ttk
 from ttkbootstrap import *
 from ttkbootstrap.style import Style
 from PIL import Image, ImageTk
 import pywhatkit as kit
-from datetime import datetime
+import datetime
+
+# mostrar mensagem = messagebox.showinfo('Mensagem!')
+
+def enviar_mensagem(mensagem, numero_telefone, hora, minuto):
+    numero_usar = f'+55{numero_de_telefone}'
+    kit.sendwhatmsg(numero_usar, mensagem, hora, minuto)
+
+    return 0
+
+def definir_minutos(minutos):
+    hora_atual = datetime.datetime.now()
+
+    nova_hora = hora_atual.hour
+    novo_minuto = hora_atual.minute + minutos
+    
+    if novo_minuto >= 60:
+        nova_hora += novo_minuto // 60
+        novo_minuto = novo_minuto % 60
+    
+    if nova_hora > 23:
+        nova_hora = nova_hora % 24
+    
+    return nova_hora, novo_minuto
+
+
+def app():
+    numero = numero_de_telefone.get()
+
+    minuto_usuario = tempo_minutos.get()
+    if minuto_usuario == '':
+        minuto_usuario = 1
+
+    mensagem = caixa_de_mensagem.get("1.0", tk.END)
+    hora, minuto = definir_minutos(int(minuto_usuario))
+    print(mensagem)
+    
+    if len(numero) == 11:
+        enviar_mensagem(mensagem=mensagem, numero_telefone=numero, hora=hora, minuto=minuto)
+    else:
+        messagebox.showinfo(title='Erro', message='Você não digitou um número válido', icon='error')
+
+
+
 
 tela = tk.Tk()
-
+"""
 def funcao_numeros_recentes():
     lista = []
-    with open('contatos_recentes.txt') as arquivos:
+    with open('numeros_recentes.txt') as arquivos:
         for linha in arquivos:
             lista.append(linha.strip())
             print(linha.strip())
     return lista
 
-
-
-
+contatos_recentes = funcao_numeros_recentes()
+var_selecao = StringVar(value="Selecione")
+"""
 logo_tk = ImageTk.PhotoImage(file='logo.png')
 tela.iconphoto(False, logo_tk)
 
-def enviar_mensagem(mensagem, numero_telefone, hora, minuto):
-
-    kit.sendwhatmsg(mensagem, numero_telefone, hora, minuto)
-    return 0
-
-
-
-
-
-botao_iniciar = tk.Button(tela, text='Programar Mensagem')
-botao_iniciar.place(x=90, y=360)
-
+tk.Label(tela, text="Número de telefone:").place(x=8, y=0)
 numero_de_telefone = tk.Entry(tela, width=30)
-numero_de_telefone.place(x=8, y=10)
+numero_de_telefone.place(x=8, y=20)
+
+tk.Label(tela, text="Tempo (minutos)").place(x=200, y=0)
+tempo_minutos = tk.Entry(tela, width=8)
+tempo_minutos.place(x=218, y=20)
 
 caixa_de_mensagem = tk.Text(tela, width=35, height=6)
-caixa_de_mensagem.place(x=8, y=55)
+caixa_de_mensagem.place(x=8, y=100)
+
+botao_iniciar = tk.Button(tela, text='Programar Mensagem', command=app)
+botao_iniciar.place(x=90, y=360)
 
 
 
@@ -46,6 +86,10 @@ caixa_de_mensagem.place(x=8, y=55)
 
 
 
+
+
+# menu_de_opcoes_contatos = tk.OptionMenu(tela, var_selecao, contatos_recentes[0], *contatos_recentes,)
+# menu_de_opcoes_contatos.place(x=135, y=45)
 
 tela.title('Mensagem Prog')
 tela.geometry('300x400')
--- teste2.py
+++ teste2.py
@@ -1,18 +1,20 @@
+import datetime
 
-def funcao_numeros_recentes():
+def definir_minutos(minutos):
+    hora_atual = datetime.datetime.now()
 
-    lista = []
+    nova_hora = hora_atual.hour
+    novo_minuto = hora_atual.minute + minutos
+    
+    if novo_minuto >= 60:
+        nova_hora += novo_minuto // 60
+        novo_minuto = novo_minuto % 60
+    
+    if nova_hora > 23:
+        nova_hora = nova_hora % 24
+    
+    return nova_hora, novo_minuto
 
-    with open('contatos_recentes.txt') as arquivos:
 
-        for linha in arquivos:
-
-            lista.append(linha.strip())
-            print(linha.strip())
-
-    return lista
-
-
-for numero in funcao_numeros_recentes():
-    print(numero)
+valor_hora, valor_minuto = definir_minutos(10)
 
